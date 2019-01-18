6410b5f0c5b1c14323a1c621ad638471f99e4598
Cody Daniel Mann <mann53@purdue.edu>
2019-01-18 09:07:33 -0500

    Fpga wrapper file working

--- source/alu_fpga.sv
+++ source/alu_fpga.sv
@@ -14,14 +14,14 @@ module alu_fpga
   (
   input logic [3:0] KEY,
   input logic [17:0] SW,
-  output logic [3:0] HEX0,
-  output logic [3:0] HEX1,
-  output logic [3:0] HEX2,
-  output logic [3:0] HEX3,
-  output logic [3:0] HEX4, 
-  output logic HEX5, 
-  output logic HEX6, 
-  output logic HEX7
+  output logic [6:0] HEX0,
+  output logic [6:0] HEX1,
+  output logic [6:0] HEX2,
+  output logic [6:0] HEX3,
+  output logic [6:0] HEX4, 
+  output logic [6:0] HEX5, 
+  output logic [6:0] HEX6, 
+  output logic [6:0] HEX7
   );
 
 // interfaces
@@ -44,126 +44,156 @@ assign aluif.alu_op[3] = ~KEY[3]; // keys are active low
   always_comb
   begin
     unique casez (aluif.result[3:0])
-      'h0: HEX0 = 4'd0;
-      'h1: HEX0 = 4'd1;
-      'h2: HEX0 = 4'd2;
-      'h3: HEX0 = 4'd3;
-      'h4: HEX0 = 4'd4;
-      'h5: HEX0 = 4'd5;
-      'h6: HEX0 = 4'd6;
-      'h7: HEX0 = 4'd7;
-      'h8: HEX0 = 4'd8;
-      'h9: HEX0 = 4'd9;
-      'ha: HEX0 = 4'd10;
-      'hb: HEX0 = 4'd11;
-      'hc: HEX0 = 4'd12;
-      'hd: HEX0 = 4'd13;
-      'he: HEX0 = 4'd14;
-      'hf: HEX0 = 4'd15;
+      'h0: HEX0 = 7'b1000000;
+      'h1: HEX0 = 7'b1111001;
+      'h2: HEX0 = 7'b0100100;
+      'h3: HEX0 = 7'b0110000;
+      'h4: HEX0 = 7'b0011001;
+      'h5: HEX0 = 7'b0010010;
+      'h6: HEX0 = 7'b0000010;
+      'h7: HEX0 = 7'b1111000;
+      'h8: HEX0 = 7'b0000000;
+      'h9: HEX0 = 7'b0010000;
+      'ha: HEX0 = 7'b0001000;
+      'hb: HEX0 = 7'b0000011;
+      'hc: HEX0 = 7'b0100111;
+      'hd: HEX0 = 7'b0100001;
+      'he: HEX0 = 7'b0000110;
+      'hf: HEX0 = 7'b0001110;
     endcase
   end
 
-    always_comb
+always_comb
   begin
     unique casez (aluif.result[7:4])
-      'h0: HEX1 = 4'd0;
-      'h1: HEX1 = 4'd1;
-      'h2: HEX1 = 4'd2;
-      'h3: HEX1 = 4'd3;
-      'h4: HEX1 = 4'd4;
-      'h5: HEX1 = 4'd5;
-      'h6: HEX1 = 4'd6;
-      'h7: HEX1 = 4'd7;
-      'h8: HEX1 = 4'd8;
-      'h9: HEX1 = 4'd9;
-      'ha: HEX1 = 4'd10;
-      'hb: HEX1 = 4'd11;
-      'hc: HEX1 = 4'd12;
-      'hd: HEX1 = 4'd13;
-      'he: HEX1 = 4'd14;
-      'hf: HEX1 = 4'd15;
+      'h0: HEX1 = 7'b1000000;
+      'h1: HEX1 = 7'b1111001;
+      'h2: HEX1 = 7'b0100100;
+      'h3: HEX1 = 7'b0110000;
+      'h4: HEX1 = 7'b0011001;
+      'h5: HEX1 = 7'b0010010;
+      'h6: HEX1 = 7'b0000010;
+      'h7: HEX1 = 7'b1111000;
+      'h8: HEX1 = 7'b0000000;
+      'h9: HEX1 = 7'b0010000;
+      'ha: HEX1 = 7'b0001000;
+      'hb: HEX1 = 7'b0000011;
+      'hc: HEX1 = 7'b0100111;
+      'hd: HEX1 = 7'b0100001;
+      'he: HEX1 = 7'b0000110;
+      'hf: HEX1 = 7'b0001110;
     endcase
   end
 
-  always_comb
+always_comb
   begin
     unique casez (aluif.result[11:8])
-      'h0: HEX2 = 4'd0;
-      'h1: HEX2 = 4'd1;
-      'h2: HEX2 = 4'd2;
-      'h3: HEX2 = 4'd3;
-      'h4: HEX2 = 4'd4;
-      'h5: HEX2 = 4'd5;
-      'h6: HEX2 = 4'd6;
-      'h7: HEX2 = 4'd7;
-      'h8: HEX2 = 4'd8;
-      'h9: HEX2 = 4'd9;
-      'ha: HEX2 = 4'd10;
-      'hb: HEX2 = 4'd11;
-      'hc: HEX2 = 4'd12;
-      'hd: HEX2 = 4'd13;
-      'he: HEX2 = 4'd14;
-      'hf: HEX2 = 4'd15;
+      'h0: HEX2 = 7'b1000000;
+      'h1: HEX2 = 7'b1111001;
+      'h2: HEX2 = 7'b0100100;
+      'h3: HEX2 = 7'b0110000;
+      'h4: HEX2 = 7'b0011001;
+      'h5: HEX2 = 7'b0010010;
+      'h6: HEX2 = 7'b0000010;
+      'h7: HEX2 = 7'b1111000;
+      'h8: HEX2 = 7'b0000000;
+      'h9: HEX2 = 7'b0010000;
+      'ha: HEX2 = 7'b0001000;
+      'hb: HEX2 = 7'b0000011;
+      'hc: HEX2 = 7'b0100111;
+      'hd: HEX2 = 7'b0100001;
+      'he: HEX2 = 7'b0000110;
+      'hf: HEX2 = 7'b0001110;
     endcase
   end
 
-  always_comb
+always_comb
   begin
     unique casez (aluif.result[15:12])
-      'h0: HEX3 = 4'd0;
-      'h1: HEX3 = 4'd1;
-      'h2: HEX3 = 4'd2;
-      'h3: HEX3 = 4'd3;
-      'h4: HEX3 = 4'd4;
-      'h5: HEX3 = 4'd5;
-      'h6: HEX3 = 4'd6;
-      'h7: HEX3 = 4'd7;
-      'h8: HEX3 = 4'd8;
-      'h9: HEX3 = 4'd9;
-      'ha: HEX3 = 4'd10;
-      'hb: HEX3 = 4'd11;
-      'hc: HEX3 = 4'd12;
-      'hd: HEX3 = 4'd13;
-      'he: HEX3 = 4'd14;
-      'hf: HEX3 = 4'd15;
+      'h0: HEX3 = 7'b1000000;
+      'h1: HEX3 = 7'b1111001;
+      'h2: HEX3 = 7'b0100100;
+      'h3: HEX3 = 7'b0110000;
+      'h4: HEX3 = 7'b0011001;
+      'h5: HEX3 = 7'b0010010;
+      'h6: HEX3 = 7'b0000010;
+      'h7: HEX3 = 7'b1111000;
+      'h8: HEX3 = 7'b0000000;
+      'h9: HEX3 = 7'b0010000;
+      'ha: HEX3 = 7'b0001000;
+      'hb: HEX3 = 7'b0000011;
+      'hc: HEX3 = 7'b0100111;
+      'hd: HEX3 = 7'b0100001;
+      'he: HEX3 = 7'b0000110;
+      'hf: HEX3 = 7'b0001110;
     endcase
   end
 
 always_comb
   begin
     unique casez (aluif.result[19:16])
-      'h0: HEX4 = 4'd0;
-      'h1: HEX4 = 4'd1;
-      'h2: HEX4 = 4'd2;
-      'h3: HEX4 = 4'd3;
-      'h4: HEX4 = 4'd4;
-      'h5: HEX4 = 4'd5;
-      'h6: HEX4 = 4'd6;
-      'h7: HEX4 = 4'd7;
-      'h8: HEX4 = 4'd8;
-      'h9: HEX4 = 4'd9;
-      'ha: HEX4 = 4'd10;
-      'hb: HEX4 = 4'd11;
-      'hc: HEX4 = 4'd12;
-      'hd: HEX4 = 4'd13;
-      'he: HEX4 = 4'd14;
-      'hf: HEX4 = 4'd15;
+      'h0: HEX4 = 7'b1000000;
+      'h1: HEX4 = 7'b1111001;
+      'h2: HEX4 = 7'b0100100;
+      'h3: HEX4 = 7'b0110000;
+      'h4: HEX4 = 7'b0011001;
+      'h5: HEX4 = 7'b0010010;
+      'h6: HEX4 = 7'b0000010;
+      'h7: HEX4 = 7'b1111000;
+      'h8: HEX4 = 7'b0000000;
+      'h9: HEX4 = 7'b0010000;
+      'ha: HEX4 = 7'b0001000;
+      'hb: HEX4 = 7'b0000011;
+      'hc: HEX4 = 7'b0100111;
+      'hd: HEX4 = 7'b0100001;
+      'he: HEX4 = 7'b0000110;
+      'hf: HEX4 = 7'b0001110;
     endcase
   end
+
 
 always_comb
   begin
     unique casez (aluif.zero)
-      'h0: HEX5 = 1'b0;
-      'h1: HEX5 = 1'b1; 
+      'h0: HEX5 = 7'b1000000;
+      'h1: HEX5 = 7'b1111001;
+      'h2: HEX5 = 7'b0100100;
+      'h3: HEX5 = 7'b0110000;
+      'h4: HEX5 = 7'b0011001;
+      'h5: HEX5 = 7'b0010010;
+      'h6: HEX5 = 7'b0000010;
+      'h7: HEX5 = 7'b1111000;
+      'h8: HEX5 = 7'b0000000;
+      'h9: HEX5 = 7'b0010000;
+      'ha: HEX5 = 7'b0001000;
+      'hb: HEX5 = 7'b0000011;
+      'hc: HEX5 = 7'b0100111;
+      'hd: HEX5 = 7'b0100001;
+      'he: HEX5 = 7'b0000110;
+      'hf: HEX5 = 7'b0001110;
     endcase
   end
+
 
 always_comb
   begin
     unique casez (aluif.negative)
-      'h0: HEX6 = 1'b0;
-      'h1: HEX6 = 1'b1; 
+      'h0: HEX6 = 7'b1000000;
+      'h1: HEX6 = 7'b1111001;
+      'h2: HEX6 = 7'b0100100;
+      'h3: HEX6 = 7'b0110000;
+      'h4: HEX6 = 7'b0011001;
+      'h5: HEX6 = 7'b0010010;
+      'h6: HEX6 = 7'b0000010;
+      'h7: HEX6 = 7'b1111000;
+      'h8: HEX6 = 7'b0000000;
+      'h9: HEX6 = 7'b0010000;
+      'ha: HEX6 = 7'b0001000;
+      'hb: HEX6 = 7'b0000011;
+      'hc: HEX6 = 7'b0100111;
+      'hd: HEX6 = 7'b0100001;
+      'he: HEX6 = 7'b0000110;
+      'hf: HEX6 = 7'b0001110;
     endcase
   end
 
@@ -171,10 +201,26 @@ always_comb
 always_comb
   begin
     unique casez (aluif.overflow)
-      'h0: HEX7 = 1'b0;
-      'h1: HEX7 = 1'b1; 
+      'h0: HEX7 = 7'b1000000;
+      'h1: HEX7 = 7'b1111001;
+      'h2: HEX7 = 7'b0100100;
+      'h3: HEX7 = 7'b0110000;
+      'h4: HEX7 = 7'b0011001;
+      'h5: HEX7 = 7'b0010010;
+      'h6: HEX7 = 7'b0000010;
+      'h7: HEX7 = 7'b1111000;
+      'h8: HEX7 = 7'b0000000;
+      'h9: HEX7 = 7'b0010000;
+      'ha: HEX7 = 7'b0001000;
+      'hb: HEX7 = 7'b0000011;
+      'hc: HEX7 = 7'b0100111;
+      'hd: HEX7 = 7'b0100001;
+      'he: HEX7 = 7'b0000110;
+      'hf: HEX7 = 7'b0001110;
     endcase
   end
+  
+
 
 // comb blocks 
 always_comb begin: REGISTER_B_NEXT_STATE; 
