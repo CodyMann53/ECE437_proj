3195b92f0bcb2c9839978c6dc4c30ef90b314ed4
Cody Daniel Mann <mann53@purdue.edu>
2019-01-21 16:33:57 -0500

    First version of memory control test bench has been implemented with very simple operations. Needs more to be able to cover more types of test cases.

--- testbench/memory_control_tb.sv
+++ testbench/memory_control_tb.sv
@@ -137,6 +137,9 @@ program test
   // declare the unpacted/dynamically sized test-vector array 
   test_vector tb_test_cases []; 
 
+  // copy of memory for checking functionality of memory control 
+  word_t [31:0] ram_copy; 
+
   /*************** task definitions *************************************/
   
   // toggles the reset line 
@@ -162,7 +165,7 @@ program test
 
   // assigns an element in array of test vectors its information 
   task add_test; 
-    input logic array_element; 
+    input int array_element; 
     input string test_name; 
     input word_t memory_address, test_data; 
     input operation_command test_type; 
@@ -176,10 +179,65 @@ program test
     end 
   endtask
 
+  task check_data; 
+    input word_t memory_address; 
+    input word_t test_data; 
+    input string test_description; 
+    begin 
+
+      // if the data returned is not what should be expected 
+      if (test_data != ram_copy[memory_address]) begin 
+
+        // send error message to both questasim and terminal 
+        $monitor("Incorrect value for test case: %s.", test_description); 
+        $monitor("Expected value: %0d. Produced value: %0d.", ram_copy[memory_address], test_data);
+        $display("Time: @%00g ns, Incorrect value for test case: %s.", test_description); 
+        $display("Expected value: %0d. Produced value: %0d.", ram_copy[memory_address], test_data); 
+      end 
+    end 
+  endtask
+
+  task write_data; 
+    input word_t test_data, memory_address; 
+    begin
+
+      // getting away from rising edge before applying inputs 
+      @(negedge CLK); 
+
+      // apply propper inputs to memory control for writing data  
+      dWEN = 1'b1; 
+      daddr = memory_address;
+      dstore = test_data; 
+
+      // wait a little to allow inputs to be applied before checking dwait 
+      #(0.5)
+
+      // wait until dwait is brought back low 
+      while (dwait == 1'b1) begin 
+        // do nothing here (just waiting)
+      end 
+
+      // get away from rising edge before deasserting inputs 
+      @(negedge CLK)
+
+      // deasert the inputs 
+      dWEN = 1'b0; 
+      daddr = 32'd0; 
+      dstore = 32'd0;
+
+      // update the copy of ram based on what was just written 
+      ram_copy[memory_address] = test_data;  
+    end 
+  endtask
+
   // task to read instruction from memory
   task read_instruction; 
     input word_t memory_address; 
+    input string test_description; 
     begin 
+
+      // get away from rising edge before applying inputs 
+      @(negedge CLK); 
 
       // apply propper inputs to memory control for a read instruciton
       iREN = 1'b1; 
@@ -194,12 +252,28 @@ program test
       end 
 
       // check for expected instruction value
-      check_data(memory_address);
+      check_data(memory_address, iload, test_description);
+
+      // get away from rising edge before deasserting inputs 
+      @(negedge CLK); 
+      
+      // deassert the inputs 
+      iREN = 1'b0; 
+      iaddr = 32'd0; 
     end 
   endtask
 
   //initial block  
   initial begin
+
+  // allocating space for test cases 
+  tb_test_cases = [1]; 
+
+  // assigning test cases to array 
+  add_test(1, "writing data to memory", 32'd0, 32'd1, WRITE_DATA); 
+
+  // initialize the copy of ram to all zero values 
+  ram_copy = 'b0; 
 
   // initialize all of the outputs to the memory controller (default values)
   nRST = 1'b0; 
@@ -210,8 +284,34 @@ program test
   iaddr = 32'd0; 
   daddr = 32'd0; 
 
+  // reset the devices under test 
+  reset_dut(); 
+
   // loop through all of the test cases
+  for (int i = 0; i < tb_test_cases.size(); i++) begin 
 
+    // update the test number and description 
+    test_num = i; 
+    test_description = tb_test_cases[i].test_name; 
 
+    // wait a little before applying next test 
+    #(1)
+
+    // if a write data test 
+    if (tb_test_cases[i].test_type == WRITE_DATA) begin 
+
+      // call write data task 
+      write_data( tb_test_cases[i].test_data, 
+                  tb_test_cases[i].memory_address
+                ); 
+    end 
+    // if a read instruction test 
+    else if (tb_test_cases[i].test_type == READ_INSTR) begin 
+
+      // call write data task 
+      write_data( tb_test_cases[i].memory_address, 
+                  tb_test_cases[i].test_name
+                ); 
+    end 
   end 
 endprogram
