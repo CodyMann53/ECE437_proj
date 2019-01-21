826b361380dba3381e2d0e295c8866046c0cc6dc
Cody Daniel Mann <mann53@purdue.edu>
2019-01-21 16:05:07 -0500

    Still trying to implement a testbench for the memory control.

--- testbench/memory_control_tb.sv
+++ testbench/memory_control_tb.sv
@@ -24,14 +24,14 @@ module memory_control_tb;
 
   // interface
   cache_control_if ccif ();
+  cpu_ram_if ramif (); 
 
-  // DUT
+  // DUT declarations 
   `ifndef MAPPED
     memory_control DUT_MEMORY_CONTROL(CLK, nRST, ccif);
     ram DUT_RAM(CLK, nRST, cpu_ram_if.ram ramif); 
-
   `else
-    memory_control DUT(
+    memory_control DUT_MEMORY_CONTROL(
       .\ccif.iREN (ccif.iREN),
       .\ccif.dREN (ccif.dREN), 
       .\ccif.dWEN (ccif.dWEN),
@@ -56,7 +56,27 @@ module memory_control_tb;
       .\nRST (nRST),
       .\CLK (CLK)
     );
+    ram DUT_RAM(
+      .\CLK (CLK),
+      .\nRST (nRST), 
+      .\ramaddr (ramif.ramaddr),
+      .\ramstore (ramif.ramstore), 
+      .\ramREN (ramif.ramREN), 
+      .\ramWEN (ramif.ramWEN), 
+      .\ramstate (ramif.ramstate), 
+      .\ramload (ramif.ramload)
+    );
   `endiff
+
+  // assign statements memory control -> ram 
+  assign ramif.ramaddr = ccif.ramaddr; 
+  assign ramif.ramREN = ccif.ramREN; 
+  assign ramif.ramWEN = ccif.ramWEN; 
+  assign ramif.ramstore = ccif.ramstore; 
+
+  // assign statements ram -> memory control 
+  assign ccif.ramload = ramif.ramload; 
+  assign ccif.ramstate = ramif.ramstate; 
 
   // test program
   test PROG ( 
@@ -79,7 +99,6 @@ module memory_control_tb;
     .ramload(ccif.ramload), 
     .ramstate(ccif.ramstate) 
     ); 
-
 endmodule
 
 program test
@@ -87,11 +106,10 @@ program test
   import cpu_types_pkg::*; 
   // modports
   (
-  input logic CLK, iwait, dwait, ramWEN, ramREN, 
-  input word_t iload, dload, ramaddr, ramstore,
+  input logic CLK, iwait, dwait,  
+  input word_t iload, dload,
   output logic nRST, iREN, dREN, dWEN, 
-  output word_t dstore, iaddr, daddr, ramload,
-  output ramstate_t ramstate
+  output word_t dstore, iaddr, daddr
   ); 
 
   // variable definitions for test case description 
@@ -106,20 +124,22 @@ program test
     READ_INSTR = 2'd0, 
     READ_DATA = 2'd1, 
     WRITE_DATA = 2'd2
-  } descriptor; 
+  } operation_command; 
 
   // test vector definitions 
   typedef struct{
     string test_name; 
     word_t memory_address; 
-    word_t data; 
-    descripto test_type;  
+    word_t test_data; 
+    operation_command test_type;  
   }  test_vector; 
 
   // declare the unpacted/dynamically sized test-vector array 
   test_vector tb_test_cases []; 
 
-  // task definitions 
+  /*************** task definitions *************************************/
+  
+  // toggles the reset line 
   task reset_dut; 
     begin 
 
@@ -140,7 +160,58 @@ program test
     end 
   endtask
 
+  // assigns an element in array of test vectors its information 
+  task add_test; 
+    input logic array_element; 
+    input string test_name; 
+    input word_t memory_address, test_data; 
+    input operation_command test_type; 
+    begin 
+
+      // pass value into array 
+      tb_test_cases[array_element].test_name = test_name; 
+      tb_test_cases[array_element].memory_address = memory_address; 
+      tb_test_cases[array_element].test_data = test_data; 
+      tb_test_cases[array_element].test_type = test_type; 
+    end 
+  endtask
+
+  // task to read instruction from memory
+  task read_instruction; 
+    input word_t memory_address; 
+    begin 
+
+      // apply propper inputs to memory control for a read instruciton
+      iREN = 1'b1; 
+      iaddr = memory_address; 
+
+      // wait a little to allow inputs to be applied before checking iwait 
+      #(0.5)
+
+      // wait until iwait is brought back low 
+      while (iwait == 1'b1) begin 
+        // do nothing here (just waiting)
+      end 
+
+      // check for expected instruction value
+      check_data(memory_address);
+    end 
+  endtask
+
   //initial block  
   initial begin
+
+  // initialize all of the outputs to the memory controller (default values)
+  nRST = 1'b0; 
+  iREN = 1'b0; 
+  dREN = 1'b0; 
+  dWEN = 1'b0; 
+  dstore = 32'd0; 
+  iaddr = 32'd0; 
+  daddr = 32'd0; 
+
+  // loop through all of the test cases
+
+
   end 
 endprogram
