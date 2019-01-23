654583d6b170cceb56430a07dc4c8edafa989e32
Cody Daniel Mann <mann53@purdue.edu>
2019-01-22 20:42:31 -0500

    Implemented the unit test assembly files

--- asmFiles/unitTest_branchInstructions.asm
+++ asmFiles/unitTest_branchInstructions.asm
@@ -5,10 +5,37 @@ org 0x0000
 
 main:
 
+	#load 2 into register $8 and $9
+	ori $8, $0, 2
+	ori $9, $0, 2
 
+	#if equal then jump to branch equal 
+	beq $8, $9, branch_eq
+
+	#jump to return (shouldn't make it here)
+	j return
+
+branch_neq:
+
+	#place 4 into register 8 to know that program ran correctly 
+	ori $8, $0, 8
+	
+	#jump to return (This is where it should jump to end program)
+	j return 
+
+return: 
+	
 	#program done 
 	halt
 
+branch_eq:
 
+	#load different values into register $8 and $9
+	ori $8, $0, 3
+	ori $9, $0, 2
 
+	#if equal then jump to branch equal 
+	bne $8, $9, branch_neq
 
+	#jump to return (shouldn't make it here)
+	j return
--- asmFiles/unitTest_jumpInstructions.asm
+++ asmFiles/unitTest_jumpInstructions.asm
@@ -5,12 +5,27 @@ org 0x0000
 
 main:
 	
+	#jump and link to func
+	jal func
 
+	#jump to return 
+	j return
 
-	#program done 
-	halt
-
+	#place 5 in register 8 (should not get here)
+	ori $8, $0, 5
 
 func:
 
+	#place 10 in register 8 to know that jr worked correctly
+	ori $8, $0, 10
 
+	#jump to return address 
+	jr $31
+
+	# place 12 in register 8 (should not get here)
+	ori $8, $0, 12
+
+return: 
+
+	#program done 
+	halt
