f93a7c9de9217ebc097f7b47facdf4e95bff31a1
Cody Daniel Mann <mann53@purdue.edu>
2019-01-30 21:44:21 -0500

    Added logic for control signals that can be done in assignment statements. Still need to write control logic for mux selection and the alu operation.

--- source/control_unit.sv
+++ source/control_unit.sv
@@ -36,16 +36,21 @@ logic [25:0] address;
 /********** Assign statements ***************************/
 
 // break up the instruction into its respective data sections
-assign op_code = instruction[32:27];
-assign funct = instruction[5:0]; 
-assign rd = instruction[16:12]; 
-assign rt = instruction[21:17];
-assign rs = instruction[26:22]; 
-assign imm_16 = instruction[15:0]; 
-assign address = instruction[25:0]; 
+assign op_code = cuif.instruction[32:27];
+assign funct = cuif.instruction[5:0]; 
+assign rd = cuif.instruction[16:12]; 
+assign rt = cuif.instruction[21:17];
+assign rs = cuif.instruction[26:22]; 
+assign imm_16 = cuif.instruction[15:0]; 
+assign address = cuif.instruction[25:0]; 
 
 // control signal logic equations 
-
+assign cuif.iREN = (op_code != HALT) ? 1 : 0; 
+assign cuif.dWEN = (op_code == SW) ? 1 : 0; 
+assign cuif.dREN = ((op_code == LUI) | (op_code == LW)) ? 1 : 0; 
+assign cuif.RegWr = ((op_code == JR) | (op_code == J) | (op_code == SW) | (op_code == BNE)
+						| (op_code == BEQ) | (op_code == JR)) ? 0 : 1; 
+assign cuif.halt = (op_code == HALT) ? 1 : 0; 
 
 /********** Combination Logic Blocks ***************************/
 
@@ -56,13 +61,13 @@ always_comb begin: INSTRUCTION_TYPE_LOGIC
 	op_code_type = I_TYPE; 
 
 	// if op code is all zeros 
-	if ( (cu.instruction[32:27] & 6'b111111) == 6'd000000) begin 
+	if ( (cuif.instruction[32:27] & 6'b111111) == 6'd000000) begin 
 
 		// instruction is R_type 
 		op_code_type = R_type; 
 	end 
 	// if the top 4 bits are zero
-	else if ( (cu.instruction[32:29] & 4'b1111) == 4'b0000) begin 
+	else if ( (cuif.instruction[32:29] & 4'b1111) == 4'b0000) begin 
 
 		// instruction is J_type 
 		op_code_type = J_type; 
@@ -73,5 +78,13 @@ always_comb begin: INSTRUCTION_TYPE_LOGIC
 		op_code_type = I_TYPE; 
 	end
 end 
+
+// mux control signal combination logic 
+always_comb begin: MUX_CONTROL_SIGNAL_LOGIC
+end 
 	
+// alu operation control logic block 
+always_comb begin: ALU_OPERATION_SIGNAL_LOGIC
+end 
+
 endmodule
