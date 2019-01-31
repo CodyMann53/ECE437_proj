8b62b3a7d60bf71694a03ab13c7e28352c45cdf7
Cody Daniel Mann <mann53@purdue.edu>
2019-01-31 11:39:39 -0500

    Control Unit is finishe. Not tested yet.

--- source/control_unit.sv
+++ source/control_unit.sv
@@ -95,7 +95,26 @@ always_comb begin: MUX_MEM_TO_REG
 	// assign default values to prevent latches
 	cuif.mem_to_reg = SEL_RESULT;
 
-	 
+	// if opcode is instruction that requires New program counter 
+	if (op_code == JAL) begin 
+
+		cuif.mem_to_reg = SEL_NPC; 
+	end 
+	// if opcode is instruction that requires data from memroy to load 
+	else if (op_code == LW) begin 
+
+		cuif.mem_to_reg = SEL_DLOAD; 
+	end 
+	// if opcode that requires imm 16 to be loaded to upper 32 bits 
+	else if (op_code == LULI) begin 
+
+		cuif.mem_to_reg = SEL_IMM16_TO_UPPER_32; 
+	end 
+	// else just send the result back to register file 
+	else begin 
+
+		cuif.mem_to_reg = SEL_RESULT; 
+	end 
 end 
 
 // mux control signal logic for alu source
@@ -167,6 +186,44 @@ always_comb begin: MUX_REG_DEST
 end 
 // alu operation control logic block 
 always_comb begin: ALU_OPERATION_SIGNAL_LOGIC
-end 
+	
+	// assign defalut value for alu operation logic signal 
+	cuif.alu_op = ALU_ADD; 
 
+	// If the instruction type is an Rtype  
+	if (op_code_type == R_TYPE) begin 
+
+		// will need to look at the function component of instruction
+		casez (funct)
+
+			SLLV: 	cuif.alu_op = ALU_SLL; 
+			SRLV: 	cuif.alu_op = ALU_SRL; 
+			ADD: 	cuif.alu_op = ALU_ADD; 
+			ADDU:	cuif.alu_op = ALU_ADD;  
+			SUB:	cuif.alu_op = ALU_SUB; 
+			SUBU:	cuif.alu_op = ALU_SUB; 
+			AND:	cuif.alu_op = ALU_AND; 
+			OR:		cuif.alu_op = ALU_OR; 
+			XOR:	cuif.alu_op = ALU_XOR; 
+			NOR:	cuif.alu_op = ALU_NOR; 
+			SLT:	cuif.alu_op = ALU_SLT;
+			SLTU:	cuif.alu_op = ALU_SLTU; 
+		endcase
+	// else will look at the opcode 
+	else begin 
+
+		casez (op_code)
+
+			BEQ:	cuif.alu_op = ALU_SUB; 
+			BNE:	cuif.alu_op = ALU_SUB; 
+			ADDI:	cuif.alu_op = ALU_ADD; 
+			ADDIU:	cuif.alu_op = ALU_ADD; 
+			SLTI:	cuif.alu_op = ALU_SLT; 
+			SLTIU:	cuif.alu_op = ALU_SLTU; 
+			ANDI:	cuif.alu_op = ALU_AND; 
+			ORI:	cuif.alu_op = ALU_OR; 
+			XORI:	cuif.alu_op = ALU_XOR; 
+			SW:		cuif.alu_op = ALU_ADD; 
+		endcase
+end 
 endmodule
