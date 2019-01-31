c24f8ac9260ecd89fb762ca2dee88964188b1a04
Cody Daniel Mann <mann53@purdue.edu>
2019-01-31 11:06:57 -0500

    Addind the logic for mux control signals.

--- source/control_unit.sv
+++ source/control_unit.sv
@@ -7,9 +7,11 @@
 
 `include "cpu_types_pkg.vh"
 `include "control_unit_if.vh"
+`include "data_path_muxs_pkg.vh"
 
 module control_unit
 	import cpu_types_pkg::*;
+	import data_path_muxs_pkg::*; 
 	(
  	control_unit_if.cu cuif, 
  	); 
@@ -51,6 +53,14 @@ assign cuif.dREN = ((op_code == LUI) | (op_code == LW)) ? 1 : 0;
 assign cuif.RegWr = ((op_code == JR) | (op_code == J) | (op_code == SW) | (op_code == BNE)
 						| (op_code == BEQ) | (op_code == JR)) ? 0 : 1; 
 assign cuif.halt = (op_code == HALT) ? 1 : 0; 
+assign cuif.extend = ((op_code == ADDIU) | (op_code == ADDI) 
+						| (op_code == ANDI) 
+						| (op_code == LW)
+						| (op_code == SLTI)
+						| (op_code == SLTIU) 
+						| (op_code == SW)
+						| (op_code == XORI)
+						| (op_code == )) ? 1 : 0; 
 
 /********** Combination Logic Blocks ***************************/
 
@@ -80,9 +90,81 @@ always_comb begin: INSTRUCTION_TYPE_LOGIC
 end 
 
 // mux control signal combination logic 
-always_comb begin: MUX_CONTROL_SIGNAL_LOGIC
+always_comb begin: MUX_MEM_TO_REG
+	
+	// assign default values to prevent latches
+	cuif.mem_to_reg = SEL_RESULT;
+
+	 
+end 
+
+// mux control signal logic for alu source
+always_comb begin: MUX_ALU_SRC
+
+	// assign default values to prevent latches 
+	cuif.ALUSrc = SEL_REG_DATA; 
+
+	// if certain cases where immediate value should be selected 
+	if ((op_code == ADDIU) | (op_code == ADDI) | (op_code == ANDI)
+		| (op_code == LW) | (op_code == ORI) | (op_code == SW) 
+		| (op_code == XORI) ) begin 
+		cuif.ALUSrc = SEL_IMM16; 
+	end 
+	else begin 
+
+		cuif.ALUSrc = SEL_REG_DATA; 
+	end 
+end
+
+// mux control signal logic for program counter source
+always_comb begin: MUX_PC_SRC
+	
+	// assign default values to prevent latches 
+	cuif.PCSrc = SEL_LOAD_ADDR; 
+
+	// opcode is bequal and equal is one  
+	if ((op_code == BEQ) & (cuif.equal == 1'b1)) begin 
+
+		cuif.PCSrc = SEL_LOAD_ADDR; 
+	end 
+	// if opcode is bneq and equal is zero 
+	else if ( (op_code == BNE) & (cuif.equal == 1'b0)) begin 
+
+		cuif.PCSrc = SEL_LOAD_ADDR; 
+	end
+	else if ((op_code == J) | (op_code == JAL)) begin 
+
+		cuif.PCSrc = SEL_LOAD_ADDR; 
+	end
+	// opcode that required pc source to be jump address
+	else if (op_code == JR) begin 
+
+		cuif.PCSrc = SEL_LOAD_JR_ADDR; 
+	end 
+	// pc should just be next instruction 
+	else begin 
+
+		cuif.PCSrc = SEL_LOAD_NXT_INSTR; 
+	end 
 end 
 	
+// mux control signal logic for selecting between rd and rt for register destination
+always_comb begin: MUX_REG_DEST
+	
+	// assign default values to prevent latches 
+	cuif.reg_dest = SEL_RD; 
+
+	// If loading values from memory
+	if ((op_code == LUI) | (op_code == LW)) begin 
+
+		// destination should be RT
+		cuif.reg_dest = SEL_RT; 
+	end 
+	else begin 
+
+		cuif.reg_dest = SEL_RD; 
+	end 
+end 
 // alu operation control logic block 
 always_comb begin: ALU_OPERATION_SIGNAL_LOGIC
 end 
