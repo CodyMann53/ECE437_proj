c2645fbe560f01e57ed538b9966882d2c812bd21
Cody Daniel Mann <mann53@purdue.edu>
2019-03-04 19:41:54 -0500

    Implemented the icache.

--- source/icache.sv
+++ source/icache.sv
@@ -22,44 +22,126 @@ module icache (
   caches_if cif
 );
 
-  word_t instr;
-  word_t daddr;
+/********** Type definitions ***************************/
+  // States for icache controller
+  typedef enum logic [1:0] {
+    IDLE = 2'd0, 
+    REQUEST = 2'd1, 
+    LOAD = 2'd2
+  }state; 
 
-  // icache
-  //icache  ICACHE(dcif, cif);
-  // dcache
-  //dcache  DCACHE(dcif, cif);
+/********** Local variable definitions ***************************/
+word_t [15:0] cache_mem_nxt, cache_mem_reg; 
+logic [25:0] [15:0] tags_nxt, tags_reg; 
+logic [15:0] vbits_nxt, vbits_reg; 
+logic [3:0] index; 
+logic [25:0] tag; 
+logic wen; 
+state state_reg, state_nxt; 
+logic hit; 
 
-  // single cycle instr saver (for memory ops)
-  always_ff @(posedge CLK)
-  begin
-    if (!nRST)
-    begin
-      instr <= '0;
-      daddr <= '0;
-    end
-    else
-    if (dcif.ihit)
-    begin
-      instr <= cif.iload;
-      daddr <= dcif.dmemaddr;
-    end
-  end
-  // dcache invalidate before halt
-  assign dcif.flushed = dcif.halt;
+/********** Assign statements ***************************/
+assign tag = dcif.imemaddr[31:6]; 
+assign index = dcif.imemaddr[5:2]; 
+assign dcif.ihit = hit; 
+assign dcif.imemload = cache_mem_reg[index]; 
+assign cif.iaddr = dcif.imemaddr; 
 
-  //singlecycle
-  assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
-  assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
-  assign dcif.imemload = cif.iload;
-  assign dcif.dmemload = cif.dload;
+/********** Combinational Logic ***************************/
+always_comb begin: CACHE_MEMORY_NEXT_STATE
+  // set default value 
+  cache_mem_nxt = cache_mem_reg; 
+  tags_nxt = tags_reg; 
+  vbits_nxt = vbits_reg; 
 
+  // if writing
+  if (wen == 1'b1) begin 
+    cache_mem_nxt[index] = cif.iload; 
+    tags_nxt[index] = tag; 
+    vbits_nxt[index] = 1'b1; 
+  end 
+end 
 
-  assign cif.iREN = dcif.imemREN;
-  assign cif.dREN = dcif.dmemREN;
-  assign cif.dWEN = dcif.dmemWEN;
-  assign cif.dstore = dcif.dmemstore;
-  assign cif.iaddr = dcif.imemaddr;
-  assign cif.daddr = dcif.dmemaddr;
+always_comb begin: CACHE_MEMORY_OUTPUT_LOGIC
+  // set default value 
+  hit = 1'b0; 
 
+  // if reading 
+  if (wen = 1'b0) begin 
+
+    // if the tags are the same and data block is valid 
+    if ((tag == tags_reg[index]) & (vbits_reg[index] == 1'b1)) begin 
+      // set hit to 1
+      hit = 1'b1; 
+    end 
+  end 
+end 
+
+always_comb begin: CONTROLLER_FSM_NXT_STATE_LOGIC 
+  // set default value
+  state_nxt = state_reg; 
+
+  // if in IDLE state
+  casez (state_reg) 
+    IDLE: begin 
+            // no read is being requested or a read is requested but no hit 
+            if ((dcif.imemREN == 0) | ((dcif.imemREN == 1) & (hit == 1'b1))) begin 
+              state_nxt = IDLE; 
+            end 
+            // reading but no hit was present 
+            else if ((dcif.imemREN == 1) & (hit == 1'b0)) begin 
+              state_nxt = REQUEST; 
+            end 
+          end 
+    REQUEST: (dcif.iwait == 1) ? state_nxt = REQUEST : state_nxt = LOAD; 
+    LOAD: state_nxt = IDLE; 
+  endcase 
+end 
+
+always_comb begin: CONTROLLER_OUTPUT_LOGIC
+  // set default values 
+  cif.iREN = 1'b0; 
+  wen = 1'b0; 
+
+  casez(state_reg) 
+    REQUEST:  begin 
+                cif.iREN = 1'b1;
+              end  
+    LOAD: begin 
+            cif.iREN = 1'b1
+            wen = 1'b1; 
+          end 
+  endcase
+end 
+
+/********** Sequential Logic ***************************/
+always_ff @(posedge CLK, negedge nRST) begin: CACHE_MEMORY_REGISTER
+  
+  // if reset is brought low 
+  if (nRST == 1'b0) begin 
+    // set tags, valid bits, and data back to zero
+    cache_mem_reg <= 'b0; 
+    tags_reg <= 'b0; 
+    vbits_reg <= 'b0; 
+  end 
+  // no reset was applied 
+  else begin 
+    // update data, tags, and valid bits 
+    cache_mem_reg <= cache_mem_nxt
+    tags_reg <= tags_nxt; 
+    vbits_reg <= vbits_nxt; 
+  end 
+end
+
+always_ff @(posedge CLK, negedge nRST) begin: CONTROLLER_FSM_REGISTER
+  
+  // if reset is brought low 
+  if (nRST == 1'b0) begin 
+    state_reg <= IDLE; 
+  end 
+  // no reset was applied 
+  else begin 
+    state_reg <= state_nxt; 
+  end 
+end
 endmodule
